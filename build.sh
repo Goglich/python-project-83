2f915069276b3d4081b112e4f287428af300c385
MyName <ivant4221@gmail.com>
2025-01-26 22:24:51 +0300

    edit build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
-if [ -f .env ]; then
-    source .env
-else
-    echo "Файл .env не найден!"
-    exit 1
-fi
-
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
 make install && psql -a -d $DATABASE_URL -f database.sql
